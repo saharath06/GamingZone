d6ee0192c7233892b8ed2c2bf714e6a2055120fd
saharath06 <saharath06@gmail.com>
2026-08-20 20:55:21 +0100

    Fix static files paths for Railway deployment

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// تفعيل الملفات الافتراضية مثل index.html
 app.UseDefaultFiles();
-app.UseStaticFiles();
+
+// تفعيل الملفات الثابتة مع تحديد مسار wwwroot بشكل صريح
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot")),
+    RequestPath = ""
+});
 
 app.Run();
